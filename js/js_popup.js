ce7f243082a6ef08de8e905f3bee4ddf1253bfb3
admin <kdy103000@naver.com>
2025-04-02 11:28:39 +0900

    4/2 5주차

---
+++ js/js_popup.js
@@ -0,0 +1,9 @@
+function pop_up() {
+    window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+    }
+
+
+
+    //상대경로 : 내가 추가한거 > js-popup.js 이런거거
+    //절대경로 : 도메인 링크 주소 > http://127.0.0.1:5500/popup/popup.html
+    // "../" 한단계 상위 폴더로 나간다는 의미 js안에서 popup 상위 폴더로 나가게해줌
